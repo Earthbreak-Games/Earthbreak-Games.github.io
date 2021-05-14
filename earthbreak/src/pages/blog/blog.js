56d572d0e1df583eda465479871543203f0d48c5
Alexander <alexanderwatersawc@gmail.com>
2021-05-14 18:11:16 -0400

    added page files

---
+++ earthbreak/src/pages/blog/blog.js
@@ -0,0 +1,12 @@
+import './landingpage.module.css';
+import LandingScreen from '../../components/landingpage/landingscreen/landingscreen'
+
+function LandingPage() {
+  return (
+    <div>
+        <LandingScreen></LandingScreen>
+    </div>
+  );
+}
+
+export default LandingPage;
