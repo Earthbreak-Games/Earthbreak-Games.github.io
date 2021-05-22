ba320a8c7b4ba8a86e7c7bd9a568f7648ee744f0
Alexander <alexanderwatersawc@gmail.com>
2021-05-22 14:03:38 -0400

    serving file via CDN

--- src/next.config.js
+++ src/next.config.js
@@ -1,21 +1,6 @@
-/*const withPlugins = require('next-compose-plugins');
-const optimizedImages = require('next-optimized-images');
+const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = {
-    // https://github.com/vercel/next.js/issues/21079
-    // Remove the workaround the issue is fixed
-    images: {
-      loader: "imgix",
-      path: ""
-    },
-};
-
-
-*/
-
-/*
-https://www.npmjs.com/package/replace-in-files-cli
-
-"build-static": "next build && next export && npm run build-static-repair-index",
-
-"build-static-repair-index": "replace-in-files --string "/_next/static" --replacement "./_next/static" out/index.html"*/
+  // Use the CDN in production and localhost for development.
+  assetPrefix: isProd ? 'https://cdn.statically.io/gh/NaveenDA/naveenda.github.io/gh-pages/' : '',
+}
