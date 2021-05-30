78a61746a719af0fa82085fad17014b3ea6115a9
alex <alexanderWatermenAWC@Gmail.com>
2021-05-30 13:34:40 -0400

    test123

--- src/next.config.js
+++ src/next.config.js
@@ -1,6 +1,13 @@
 const isProd = process.env.NODE_ENV === 'production'
-
+/*
 module.exports = {
   // Use the CDN in production and localhost for development.
   assetPrefix: isProd ? 'https://cdn.statically.io/gh/Earthbreak-Games/Earthbreak-Games.github.io/gh-pages/' : '',
+}*/
+
+
+module.exports = {
+  images: {
+    loader: 'imgix'
+  },
 }
