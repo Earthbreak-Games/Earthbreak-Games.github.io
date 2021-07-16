f89229d2eb652e32bcc0c106a27932c77d35cc36
Alexander <alexanderwatersawc@gmail.com>
2021-07-16 19:57:16 -0400

    added stuff

--- earthbreak/src/pages/contact/contactpage.js
+++ earthbreak/src/pages/contact/contactpage.js
@@ -1,11 +1,9 @@
 import './contact.module.css';
-import LandingScreen from '../../components/landingpage/landingscreen/landingscreen'
 
 function ContactPage() {
   return (
     <div>
         <p>Follow us</p>
-        
     </div>
   );
 }
